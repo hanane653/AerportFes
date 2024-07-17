d9dcf75450d4bcb1e4be6a0f0dae5a8eb46bcf21
hanane653 <hanane.amtdi@usmba.ac.ma>
2024-07-17 14:28:36 +0100

    Configure Vite to use the PORT environment variable

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 3000, // Use PORT from environment variables or fallback to 3000
+    host: true, // Use this to expose the server on the network
+  },
 })
